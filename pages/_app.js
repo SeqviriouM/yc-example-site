6df03ea812ed759a3ee094ff415649603113555b
Roman <seqvirioum@yandex-team.ru>
2023-02-17 14:08:14 +0300

    fix eslint rule

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import '@gravity-ui/uikit/styles/styles.css';
+// eslint-disable-next-line import/order
 import '../styles/globals.css';
 
 // This default export is required in a new `pages/_app.js` file.
